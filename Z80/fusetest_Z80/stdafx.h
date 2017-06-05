633ba49784547a571d5fcfb568505daa09669bf7
Adrian.Conlon <adrian.conlon@gmail.com>
2017-06-05 23:32:35 +0100

    Tidy up fuse test precompiled headers and properly place the location of the test files.
    Signed-off-by: Adrian.Conlon <adrian.conlon@gmail.com>

--- Z80/fusetest_Z80/stdafx.h
+++ Z80/fusetest_Z80/stdafx.h
@@ -17,26 +17,3 @@
 #include <array>
 #include <vector>
 #include <bitset>
-
-//#include <boost/format.hpp>
-//
-//#include <SDL.h>
-//#include <SDL_mixer.h>
-
-//#if SDL_BYTEORDER == SDL_LIL_ENDIAN
-//#define HOST_LITTLE_ENDIAN
-//#endif
-//
-//#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-//#define HOST_BIG_ENDIAN
-//#endif
-//
-//#define RUN_TESTS
-////#define RUN_CPM
-////#define RUN_INVADERS
-//
-//#ifdef _MSC_VER
-//#pragma comment(lib, "SDL2.lib")
-//#pragma comment(lib, "SDL2main.lib")
-//#pragma comment(lib, "SDL2_mixer.lib")
-//#endif
