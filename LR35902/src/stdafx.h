1c8d842bde012e8070ca36c3dfc60eb9a53d3b01
Adrian.Conlon <adrian.conlon@gmail.com>
2017-06-09 10:29:28 +0100

    Tidy header usage a little.
    Signed-off-by: Adrian.Conlon <adrian.conlon@gmail.com>

--- LR35902/src/stdafx.h
+++ LR35902/src/stdafx.h
@@ -20,18 +20,3 @@
 #include <iomanip>
 
 #include <boost/format.hpp>
-
-#include <SDL.h>
-
-#if SDL_BYTEORDER == SDL_LIL_ENDIAN
-#define HOST_LITTLE_ENDIAN
-#endif
-
-#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-#define HOST_BIG_ENDIAN
-#endif
-
-#ifdef _MSC_VER
-#pragma comment(lib, "SDL2.lib")
-#pragma comment(lib, "SDL2main.lib")
-#endif
