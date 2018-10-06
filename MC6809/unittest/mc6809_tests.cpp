7e57efd4cda67707a7a79e74aa5b7a98bef65198
Adrian Conlon <Adrian.conlon@gmail.com>
2018-10-06 23:54:33 +0100

    Modification of unit test comments: no functional modification.

--- MC6809/unittest/mc6809_tests.cpp
+++ MC6809/unittest/mc6809_tests.cpp
@@ -2,13 +2,14 @@
 #include "Board.h"
 
 // Using examples from 6809 Assembly Language Programming, by Lance A. Leventhal
+// Just test the basics...
 
 TEST_CASE("Add Accumulator B to Index Register X Unsigned ", "[ABX]") {
 
 	Board board;
 	board.initialise();
 	auto& cpu = board.CPU();
-	cpu.step();	// Jump over the reset
+	cpu.step();	// Step over the reset
 
     SECTION("Inherent") {
 		board.poke(0, 0x3a);
@@ -18,37 +19,3 @@ TEST_CASE("Add Accumulator B to Index Register X Unsigned ", "[ABX]") {
 		REQUIRE(cpu.X() == 0x111b);
     }
 }
-
-
-//TEST_CASE( "vectors can be sized and resized", "[vector]" ) {
-//
-//    std::vector<int> v( 5 );
-//    
-//    REQUIRE( v.size() == 5 );
-//    REQUIRE( v.capacity() >= 5 );
-//    
-//    SECTION( "resizing bigger changes size and capacity" ) {
-//        v.resize( 10 );
-//        
-//        REQUIRE( v.size() == 10 );
-//        REQUIRE( v.capacity() >= 10 );
-//    }
-//    SECTION( "resizing smaller changes size but not capacity" ) {
-//        v.resize( 0 );
-//        
-//        REQUIRE( v.size() == 0 );
-//        REQUIRE( v.capacity() >= 5 );
-//    }
-//    SECTION( "reserving bigger changes capacity but not size" ) {
-//        v.reserve( 10 );
-//        
-//        REQUIRE( v.size() == 5 );
-//        REQUIRE( v.capacity() >= 10 );
-//    }
-//    SECTION( "reserving smaller does not change size or capacity" ) {
-//        v.reserve( 0 );
-//        
-//        REQUIRE( v.size() == 5 );
-//        REQUIRE( v.capacity() >= 5 );
-//    }
-//}
