30b61af82068f3350b55e998846b316eeaaedae0
Adrian Conlon <adrian.conlon@gmail.com>
2021-12-08 19:50:45 +0000

    Whoops: missed this from the last commit!

--- MC6809/unittest/Board.h
+++ MC6809/unittest/Board.h
@@ -9,7 +9,7 @@ class Board final : public EightBit::Bus {
 public:
 	Board();
 
-	EightBit::mc6809& CPU() { return m_cpu; }
+	constexpr EightBit::mc6809& CPU() noexcept { return m_cpu; }
 
 	void raisePOWER() final;
 	void lowerPOWER() final;
@@ -21,8 +21,8 @@ protected:
 
 private:
 	EightBit::Ram m_ram = 0x10000;	// 0000 - FFFF, 64K RAM
-	EightBit::mc6809 m_cpu;
-	EightBit::Disassembly m_disassembler;
+	EightBit::mc6809 m_cpu = { *this };
+	EightBit::Disassembly m_disassembler = { *this, m_cpu };
 
 	// The m_disassembleAt and m_ignoreDisassembly are used to skip pin events
 	EightBit::register16_t m_disassembleAt = 0x0000;
