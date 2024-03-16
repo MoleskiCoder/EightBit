f19e67875e5a2f2bdea3ac276ad93fc8f5e44fb5
Adrian Conlon <98398945+AdrianConlon@users.noreply.github.com>
2024-03-16 14:19:28 +0000

    More addressing / read tidying

--- M6502/inc/mos6502.h
+++ M6502/inc/mos6502.h
@@ -24,9 +24,9 @@ namespace EightBit {
 	public:
 		enum StatusBits {
 			NF = Bit7,	// Negative
-			VF = Bit6,  // Overflow
-			RF = Bit5,  // reserved
-			BF = Bit4,  // Brk
+			VF = Bit6,	// Overflow
+			RF = Bit5,	// reserved
+			BF = Bit4,	// Brk
 			DF = Bit3,	// D (use BCD for arithmetic)
 			IF = Bit2,	// I (IRQ disable)
 			ZF = Bit1,	// Zero
@@ -58,21 +58,21 @@ namespace EightBit {
 
 		// Instructions with BCD effects
 
+		void sbc() noexcept;
 		[[nodiscard]] virtual uint8_t sub(uint8_t operand, int borrow = 0) noexcept;
-		[[nodiscard]] void sbc() noexcept;
 		[[nodiscard]] uint8_t sub_b(uint8_t operand, uint8_t data, int borrow = 0) noexcept;
 		[[nodiscard]] uint8_t sub_d(uint8_t operand, uint8_t data, int borrow = 0) noexcept;
 
+		void adc() noexcept;
 		[[nodiscard]] virtual uint8_t add(uint8_t operand, int carry = 0) noexcept;
-		[[nodiscard]] void adc() noexcept;
 		[[nodiscard]] uint8_t add_b(uint8_t operand, uint8_t data, int carry) noexcept;
 		[[nodiscard]] uint8_t add_d(uint8_t operand, uint8_t data, int carry) noexcept;
 
 		// Undocumented compound instructions (with BCD effects)
 
 		virtual void arr() noexcept;
-		virtual void arr_b(uint8_t value) noexcept;
-		virtual void arr_d(uint8_t value) noexcept;
+		void arr_b(uint8_t value) noexcept;
+		void arr_d(uint8_t value) noexcept;
 
 	private:
 		const uint8_t IRQvector = 0xfe;		// IRQ vector
@@ -105,19 +105,23 @@ namespace EightBit {
 
 		// Addressing modes
 
-		void Address_Immediate() noexcept { BUS().ADDRESS() = PC()++; }
+		constexpr void noteUnfixedPage() noexcept { m_unfixed_page = BUS().ADDRESS().high; }
+
+		constexpr void Address_Immediate() noexcept { BUS().ADDRESS() = PC()++; }
 		void Address_Absolute() noexcept { BUS().ADDRESS() = fetchWord(); }
 		void Address_ZeroPage() noexcept { BUS().ADDRESS() = register16_t(fetchByte(), 0); }
-		void Address_ZeroPageIndirect() noexcept;
-		void Address_Indirect() noexcept;
-		void Address_ZeroPageX() noexcept;
-		void Address_ZeroPageY() noexcept;
-		void Address_AbsoluteX() noexcept;
-		void Address_AbsoluteY() noexcept;
-		void Address_IndexedIndirectX() noexcept;
-		void Address_IndirectIndexedY() noexcept;
+		void Address_ZeroPageIndirect() noexcept { Address_ZeroPage(); BUS().ADDRESS() = getWordPaged(); }
+		void Address_Indirect() noexcept { Address_Absolute(); BUS().ADDRESS() = getWordPaged(); }
+		void Address_ZeroPageWithIndex(uint8_t index) noexcept { AM_ZeroPage(); BUS().ADDRESS().low += index; }
+		void Address_ZeroPageX() noexcept { Address_ZeroPageWithIndex(X()); }
+		void Address_ZeroPageY() noexcept { Address_ZeroPageWithIndex(Y()); }
+		void Address_AbsoluteWithIndex(uint8_t index) noexcept { Address_Absolute(); noteUnfixedPage(); BUS().ADDRESS() += index; }
+		void Address_AbsoluteX() noexcept { Address_AbsoluteWithIndex(X()); }
+		void Address_AbsoluteY() noexcept { Address_AbsoluteWithIndex(Y()); }
+		void Address_IndexedIndirectX() noexcept { Address_ZeroPageX(); BUS().ADDRESS() = getWordPaged(); }
+		void Address_IndirectIndexedY() noexcept { Address_ZeroPageIndirect(); noteUnfixedPage(); BUS().ADDRESS() += Y(); }
 
-		// Addressing modes, read
+		// Addressing modes, with read
 
 		void AM_Immediate() noexcept { Address_Immediate(); memoryRead(); }
 		void AM_Absolute() noexcept { Address_Absolute(); memoryRead(); }
@@ -125,9 +129,9 @@ namespace EightBit {
 		void AM_ZeroPageX() noexcept { Address_ZeroPageX(); memoryRead(); }
 		void AM_ZeroPageY() noexcept { Address_ZeroPageY(); memoryRead(); }
 		void AM_IndexedIndirectX() noexcept { Address_IndexedIndirectX(); memoryRead(); }
-		void AM_AbsoluteX() noexcept { Address_AbsoluteX(); maybe_fixup(); memoryRead(); }
-		void AM_AbsoluteY() noexcept { Address_AbsoluteY(); maybe_fixup(); memoryRead(); }
-		void AM_IndirectIndexedY() noexcept { Address_IndirectIndexedY(); maybe_fixup(); memoryRead(); }
+		void AM_AbsoluteX() noexcept { Address_AbsoluteX(); maybe_fixupR(); }
+		void AM_AbsoluteY() noexcept { Address_AbsoluteY(); maybe_fixupR(); }
+		void AM_IndirectIndexedY() noexcept { Address_IndirectIndexedY(); maybe_fixupR(); }
 
 		// Flag checking
 
@@ -173,9 +177,8 @@ namespace EightBit {
 			return data;
 		}
 
-#define RMW(OPERATION) { \
-			const auto data = memoryRead(); \
-			const auto result = OPERATION(data); \
+#define MW(OPERATION) { \
+			const auto result = OPERATION(BUS().DATA()); \
 			memoryWrite(); \
 			memoryWrite(result); \
 		}
@@ -196,6 +199,9 @@ namespace EightBit {
 			BUS().ADDRESS().high = fixed_page;
 		}
 
+		void maybe_fixupR() noexcept { maybe_fixup(); memoryRead(); }
+		void fixupR() noexcept { fixup(); memoryRead(); }
+
 		// Status flag operations
 
 		constexpr static void set_flag(uint8_t& f, int which, int condition) noexcept { f = setBit(f, which, condition); }
@@ -214,7 +220,7 @@ namespace EightBit {
 		// Instruction implementations
 
 		void andr() noexcept;
-		void bit(uint8_t operand) noexcept;
+		void bit() noexcept;
 		void cmp(uint8_t first) noexcept;
 		[[nodiscard]] uint8_t dec(uint8_t value) noexcept;
 		void eorr() noexcept;
@@ -227,7 +233,7 @@ namespace EightBit {
 		void rts() noexcept;
 
 		[[nodiscard]] constexpr uint8_t asl(uint8_t value) noexcept {
-			set_flag(CF, value & Bit7);
+			set_flag(CF, value & NF);
 			return through(value << 1);
 		}
 
@@ -237,7 +243,7 @@ namespace EightBit {
 		}
 
 		[[nodiscard]] constexpr uint8_t lsr(uint8_t value) noexcept {
-			set_flag(CF, value & Bit0);
+			set_flag(CF, value & CF);
 			return through(value >> 1);
 		}
 
@@ -252,20 +258,23 @@ namespace EightBit {
 		void axs() noexcept;
 		void jam() noexcept;
 
-		void sha() noexcept { memoryWrite(A() & X() & (BUS().ADDRESS().high + 1)); }
+		void storeFixupEffect(uint8_t data) noexcept { memoryWrite(data & (BUS().ADDRESS().high + 1)); }
+
+		void sha() noexcept { storeFixupEffect(A() & X()); }
+		void sya() noexcept { storeFixupEffect(Y()); }
+		void sxa() noexcept { storeFixupEffect(X()); }
 		void tas() noexcept { S() = A() & X(); sha(); }
 		void las() noexcept { A() = X() = S() = through(memoryRead() & S()); }
-		void sya() noexcept { memoryWrite(Y() & (BUS().ADDRESS().high + 1)); }
-		void sxa() noexcept { memoryWrite(X() & (BUS().ADDRESS().high + 1)); }
 		void ane() noexcept { A() = through((A() | 0xee) & X() & BUS().DATA()); }
 		void atx() noexcept { A() = X() = through((A() | 0xee) & BUS().DATA()); }
+		void asr() noexcept { andr(); A() = lsr(A()); }
 
-		void isb() noexcept { RMW(inc); sbc(); }
-		void slo() noexcept { RMW(asl); orr(); }
-		void rla() noexcept { RMW(rol); andr(); }
-		void sre() noexcept { RMW(lsr); eorr(); }
-		void rra() noexcept { RMW(ror); adc(); }
-		void dcp() noexcept { RMW(dec); cmp(A()); }
+		void isb() noexcept { MW(inc); sbc(); }
+		void slo() noexcept { MW(asl); orr(); }
+		void rla() noexcept { MW(rol); andr(); }
+		void sre() noexcept { MW(lsr); eorr(); }
+		void rra() noexcept { MW(ror); adc(); }
+		void dcp() noexcept { MW(dec); cmp(A()); }
 
 		uint8_t m_x = 0;		// index register X
 		uint8_t m_y = 0;		// index register Y
